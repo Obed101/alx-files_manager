16f1e140ba178d669e166bb7b55466b25b25e9f8
Obed Amoako <obedamoako92@gmail.com>
2022-06-10 18:11:19 +0000

    ceated redisClient class

---
+++ utils/redis.js
@@ -0,0 +1,18 @@
+const { createClient } = require("redis");
+const client = createClient();
+
+class RedisClient {
+  constructor() {
+    client.on("error", error => console.log(error));
+  }
+  isAlive() {
+    client.on("success", () => true);
+    return false;
+  }
+  async get(key) {
+    client.get(key);
+  }
+  async set(key, value, duration) {
+    setTimeout(client.set(key, value), duration);
+  }
+}
