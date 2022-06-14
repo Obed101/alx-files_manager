d7e4fcc4ab11cca2fed47631650441a958288228
Obed Amoako <obedamoako92@gmail.com>
2022-06-14 09:23:37 +0000

    changed to module

--- utils/redis.js
+++ utils/redis.js
@@ -1,24 +1,24 @@
 const { createClient } = require("redis");
-const client = createClient();
 
 class RedisClient {
   constructor() {
-    client.on("error", error => console.log(error));
+    this.client = createClient();
+    this.client.on("error", error => console.log(error));
   }
   isAlive() {
-    client.on("success", () => true);
+    this.client.on("success", () => true);
     return false;
   }
   async get(key) {
-    client.get(key);
+    this.client.get(key);
   }
   async set(key, value, duration) {
-    client.set(key, value, 'EX', duration);
+    this.client.set(key, value, 'EX', duration);
   }
   async del(key) {
-    client.del(key);
+    this.client.del(key);
   }
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
