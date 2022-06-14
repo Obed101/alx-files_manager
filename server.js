a56e7607792a40ed502482045a37ccef15073a7b
Obed Amoako <obedamoako92@gmail.com>
2022-06-14 21:55:25 +0000

    changed server activities

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import router from './routes/index.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(express.json());
-// app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(port, () => {
+app.listen(port, '127.0.0.1', () => {
   console.log(`Server running on port ${port}`);
 });
