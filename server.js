0c8f0508e04129b953c3cca45a1a0bfbe58eac36
Obed101 <obedamoako92@gmail.com>
2022-06-18 13:15:52 +0000

    modified server

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import router from './routes/index.js';
+import router from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
